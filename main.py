9fd81c0f6b9faf808b3da4a19d913b3d2efaae73
MaccSob <71sobmaciej@gmail.com>
2026-04-21 23:58:23 +0200

    httpx test

---
+++ main.py
@@ -0,0 +1,4 @@
+import httpx
+
+
+r= httpx.get('https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/malopolskie/krakow/krakow/krakow?limit=36&priceMax=3000&roomsNumber=%5BONE%2CTWO%2CTHREE%5D&by=DEFAULT&direction=DESC')
